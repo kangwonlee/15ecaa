e66f0871a360882b820b7c8496bd0784bead3c62
KangWon LEE <kangwon.lee@kpu.ac.kr>
2015-09-02 12:50:01 +0900

    4* pi -> 2 * 2 * pi sine 파 주파수를 더 분명히 표시하기 위함

--- wk01/fill_demo.py
+++ wk01/fill_demo.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(0, 1)
-y = np.sin(4 * np.pi * x) * np.exp(-5 * x)
+y = np.sin(2 * 2 * np.pi * x) * np.exp(-5 * x)
 
 plt.fill(x, y, 'r')
 plt.grid(True)
