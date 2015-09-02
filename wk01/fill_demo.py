7afbdc0bba89a423b74825932ff8d1040f5f6395
KangWon LEE <kangwon.lee@kpu.ac.kr>
2015-09-02 12:56:01 +0900

    0~1 사이 간격 수를 50개에서 100개로 늘림. 50개로는 10주기 표시가 거칠었음. 500개로는 sine파가 부드럽게 보임

--- wk01/fill_demo.py
+++ wk01/fill_demo.py
@@ -4,7 +4,7 @@ Simple demo of the fill function.
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(0, 1)
+x = np.linspace(0, 1, 500)
 y = np.sin(10 * 2 * np.pi * x) * np.exp(-5 * x)
 
 plt.fill(x, y, 'r')
