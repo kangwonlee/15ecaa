6f90bd20698f18ac3191c445192d09275652cc2b
Kangwon Lee <kangwon.lee@kpu.ac.kr>
2015-09-25 14:28:11 +0900

    trapezoid1() only

--- wk04/num_int.py
+++ wk04/num_int.py
@@ -93,66 +93,6 @@ def trapezoid1(f, x0, x1, n=100):
 # end of function trapezoid1()
 
 
-def simpson2(f, x0, x1, n=100):
-    """
-    Numerical integration
-
-    Assume f(x) is a 2nd order polynomial between x[k] and x[k+2]
-
-    Parameters
-    ----------
-    f:function to be integrated
-    x0:lower bound of integration
-    x1:upper bound of integration
-    n:number of intervals
-
-    Return:
-    ------
-    Numerical integration of function f(x) in interval [x0, x1]
-    """
-    # initialization
-    # calculate x interval
-
-    print "n =", n
-    print "n%2 =", n%2
-
-    # if n is an odd number make it an even number
-    if (n%2) : n += 1
-    delta_x = (float(x1) - float(x0)) / n
-
-    xk = x0
-    fxk = f(xk)
-
-    # integration result
-    result = 0.0
-
-    # for each two-interval
-    # k = 0, 2, 4, ..., (n-1)
-    for k in xrange(0, n, 2):
-        # k+1-th x
-        xk1 = xk + delta_x
-        # k+1-th f(x)
-        fxk1 = f(xk1)
-
-        # k+2-th x
-        xk2 = xk1 + delta_x
-        # k+2-th f(x)
-        fxk2 = f(xk2)
-
-        # k-th area
-        F_k = (fxk + 4*fxk1 + fxk2) * (delta_x / 3.0)
-
-        # accumulate to integration result
-        result += F_k
-        xk = xk2
-        fxk = fxk2
-    # end k loop
-
-    # return integration result
-    return  result
-# end of function simpson2()
-
-
 def func(x):
     return exp(x)
 # end of function func()
@@ -170,7 +110,7 @@ if "__main__" == __name__:
     # final value
     x_end = 1.0
     # number of intervals
-    n_interval = 8
+    n_interval = 2
 
     # theoretical exact solution
     exact = (Func(x_end) - Func(x_begin))
@@ -183,10 +123,6 @@ if "__main__" == __name__:
     # call trapezoid1 function
     F_1 = trapezoid1(func, x_begin, x_end, n_interval)
     print "F_1 =", F_1, "err =", F_1 - exact
-
-    # call simpson2 function
-    F_2 = simpson2(func, x_begin, x_end, n_interval)
-    print "F_2 =", F_2, "err =", F_2 - exact
 
     from pylab import fill, bar, show, xlim, ylim, grid
     # exact
