a5ac752be5c8c82085ae4fe0fb21e9ebdd8cbf3c
KangWon LEE <kangwon.lee@kpu.ac.kr>
2015-10-07 10:26:28 +0900

    과도한 지출을 방지하는 NoOverCharge class

--- oop/over_charge_protected_traffic_card.py
+++ oop/over_charge_protected_traffic_card.py
@@ -1,29 +1,24 @@
-# make TrafficCardClass based on another class named "object"
-class TrafficCardClass(object):
-    def __init__(self):
-        self.balance = 0
+import traffic_card_class as tcc
+# because name is traffic_card_class too long, use as "tcc" instead
 
-    def charge(self, amount):
-        self.balance += amount
-        # no return value -> None
 
+# make NoOverChargeCard based on tcc.TrafficCardClass : inheritance
+# class TrafficCardClass is in tcc == traffic_card_class.py
+class NoOverChargeCard(tcc.TrafficCardClass):
     def pay(self, amount):
-        self.balance -= amount
-
-    def check(self):
-        return self.balance
+        # "override" pay() "method" of TrafficCardClass
+        if 0 > (self.balance - amount):
+            print "error : not enough balance"
+            self.balance = 0
+        else:
+            self.balance -= amount
 
 if "__main__" == __name__:
-    my_card = TrafficCardClass()
-
-    # no direct access to my_card.balance : encapsulation
+    my_card = NoOverChargeCard()
     print "my_card.check() =", my_card.check()
     print "my_card.charge(10000) =", my_card.charge(10000)
     print "my_card.pay(1250) =", my_card.pay(1250)
     print "my_card.check() =", my_card.check()
-
-    your_card = TrafficCardClass()
-    print "your_card.check() =", your_card.check()
 
     print "my_card.pay(1250) =", my_card.pay(1250)
     print "my_card.check() =", my_card.check()
