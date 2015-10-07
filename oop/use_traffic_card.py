f759dacb4eca47fa111b6078dfa34eb7e80e468e
KangWon LEE <kangwon.lee@kpu.ac.kr>
2015-10-07 10:01:59 +0900

    import traffic_card as your_card

--- oop/use_traffic_card.py
+++ oop/use_traffic_card.py
@@ -4,3 +4,6 @@ print "my_card.check() =", my_card.check()
 print "my_card.charge(10000) =", my_card.charge(10000)
 print "my_card.pay(1250) =", my_card.pay(1250)
 print "my_card.check() =", my_card.check()
+
+import traffic_card as your_card
+print "your_card.check() =", your_card.check()
