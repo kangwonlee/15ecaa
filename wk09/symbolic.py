57c77b001cc7a4da35eb8c9c667c80f3f1997344
KangWon LEE <kangwon.lee@kpu.ac.kr>
2015-10-28 10:38:45 +0900

    sympy example http://www.scipy-lectures.org/advanced/sympy.html

---
+++ wk09/symbolic.py
@@ -0,0 +1,21 @@
+# -*- coding: cp949 -*-
+from sympy import *
+a = Rational(1, 2)
+print "a =", a
+
+print "pi**2 =", pi**2
+
+print "pi.evalf() =", pi.evalf()
+
+print "pi + exp(1) =", pi + exp(1)
+
+print "(pi + exp(1)).evalf() =", (pi + exp(1)).evalf()
+
+print "oo =", oo
+
+print "oo > 99999 =", oo > 99999
+
+print "sqrt(2) =", sqrt(2).evalf(100)
+
+print "1/2 + 1/3 =", Rational(1, 2) + Rational(1, 3)
+
