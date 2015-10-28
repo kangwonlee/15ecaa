8bc2ba2096adfcf21611843c40a493939826cd9c
KangWon LEE <kangwon.lee@kpu.ac.kr>
2015-10-28 11:32:08 +0900

    전역 변수 force_N : 이렇게 하지 말 것

--- wk09/yield.py
+++ wk09/yield.py
@@ -3,6 +3,9 @@ import root_finding
 
 
 def main():
+    global force_N
+    force_N = float(raw_input("Enter force (N):"))
+
     x_l_init = root_finding.epsilon * 2
     x_h_init = 1.0
     result = root_finding.bisection(problem_to_solve, x_l_init, x_h_init, 1e-9)
@@ -10,14 +13,14 @@ def main():
 
 
 def problem_to_solve(radius_m):
-    force_N = 100.0
+    global force_N
     stress_max_Pa = 207e6
     safety_factor = 2.0
     return circular_section_stress(radius_m, force_N) - stress_max_Pa / safety_factor
 
 
 def df_dr(radius_m):
-    force_N = 100.0
+    global force_N
     result = (force_N + (-2.0) / math.pi) / (radius_m**3)
     return result
 
