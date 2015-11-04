db4d303b4d44837994a90e3b36efec4d6086a635
KangWon LEE <kangwon.lee@kpu.ac.kr>
2015-11-04 10:17:47 +0900

    http://www.scipy-lectures.org/advanced/sympy.html 예제 ~2.10.3.3

--- wk09/symbolic.py
+++ wk09/symbolic.py
@@ -26,3 +26,55 @@ sigma = F / area
 solution = solve([sigma - sigma_max/sf], r)
 print "solution =", simplify(solution)
 
+'''2.10.1.3'''
+
+x = Symbol('x')
+y = Symbol('y')
+
+print "x+y+x-y =", x+y+x-y
+
+print "(x+y)**2 =", (x+y)**2
+
+'''2.10.2.1'''
+
+print "expand((x+y)**3) =", expand((x+y)**3)
+
+print "expand(x+y, complex=True) =", expand(x+y,complex=True)
+
+print "expand(cos(x+y), trig=True) =", expand(cos(x+y), trig=True)
+
+'''2.10.2.2'''
+
+print "simplify((x+x*y)/x) =", simplify((x+x*y)/x)
+
+'''2.10.3.1'''
+print "limit(sin(x)/x,x,0) =", limit(sin(x)/x, x, 0)
+
+print "limit(x,x,oo) =", limit(x, x, oo)
+
+print "limit(1/x,x,oo) =", limit(1/x, x, oo)
+
+print "limit(x**x,x,0) =", limit(x**x, x, 0)
+
+'''2.10.3.2'''
+
+print "diff(sin(x), x) =", diff(sin(x), x)
+
+print "diff(sin(2*x), x) =", diff(sin(2*x), x)
+
+print "diff(tan(x), x) =", diff(tan(x), x)
+
+print "limit((tan(x+y)-tan(x))/y,y,0) =", limit((tan(x+y)-tan(x))/y,y,0)
+
+print "diff(sin(2*x), x, 1) =", diff(sin(2*x), x, 1)
+
+print "diff(sin(2*x), x, 2) =", diff(sin(2*x), x, 2)
+
+print "diff(sin(2*x), x, 3) =", diff(sin(2*x), x, 3)
+
+'''2.10.3.3'''
+
+print "series(cos(x), x) =", series(cos(x), x)
+
+print "series(1/cos(x), x) =", series(1/cos(x), x)
+
