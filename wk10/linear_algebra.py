0cdaa9bb875afe7b69deb30cb7b0f481f8ac4585
KangWon LEE <kangwon.lee@kpu.ac.kr>
2015-11-04 12:46:12 +0900

    행렬과 벡터의 곱셈

--- wk10/linear_algebra.py
+++ wk10/linear_algebra.py
@@ -13,6 +13,19 @@ def dot(a, b):
     return result
 
 
+def multiply_matrix_vector(A, x):
+    n_row = len(A)
+    n_column = len(A[0])
+
+    result = [0.0] * n_row
+    for i in xrange(n_row):
+        result[i] = 0.0
+        for j in xrange(n_column):
+            result[i] += A[i][j] * x[j]
+
+    return result
+
+
 def main():
     a_vector = [1.0, 0.0]
     b_vector = [3.0, 4.0]
@@ -22,6 +35,14 @@ def main():
     print "b =", b_vector
     print "a dot b =", a_dot_b
 
+    A_matrix = [[0.0, 1.0],
+                [1.0, 0.0]]
+    x_vector = [3.0, 4.0]
+    A_x = multiply_matrix_vector(A_matrix, x_vector)
+
+    print "A =", A_matrix
+    print "x =", x_vector
+    print "A*x =", A_x
 
 if "__main__" == __name__:
     main()
