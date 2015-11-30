beded260852f4fae12ef8370ee27ef4a8664f05e
KangWon LEE <kangwon.lee@kpu.ac.kr>
2015-12-01 01:07:27 +0900

    added main()

--- wk09/symbolic_cholesky_decomposition.py
+++ wk09/symbolic_cholesky_decomposition.py
@@ -1,12 +1,18 @@
 import sympy as sp
 
-# Brendan Wood, MRocklin, Amelio Vazquez-Reina, Automatically populating matrix elements in SymPy, http://stackoverflow.com/questions/6877061/automatically-populating-matrix-elements-in-sympy, 2011 Aug 02, (accessed 2015 Dec 01)
 
-M = sp.Matrix(3, 3, lambda i, j: sp.var('M_%d%d' % (i+1,j+1)))
+def main():
+    # Brendan Wood, MRocklin, Amelio Vazquez-Reina, Automatically populating matrix elements in SymPy,
+    # http://stackoverflow.com/questions/6877061/automatically-populating-matrix-elements-in-sympy, 2011 Aug 02,
+    # (accessed 2015 Dec 01)
+    M = sp.Matrix(3, 3, lambda i, j: sp.var('M_%d%d' % (i+1,j+1)))
+
+    for i in xrange(2):
+        for j in xrange(i+1, 3):
+            M[i, j] = 0
+
+    print(M)
 
 
-for i in xrange(2):
-    for j in xrange(i+1, 3):
-        M[i, j] = 0
-
-print(M)
+if "__main__" == __name__:
+    main()
