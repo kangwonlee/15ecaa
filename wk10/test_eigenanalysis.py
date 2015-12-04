9e53e90bff719b00f0e618059addd7a75aa6c82c
Kangwon Lee <kangwon.lee@kpu.ac.kr>
2015-12-04 12:41:36 +0900

    integer test cases for Cholesky Factorization

--- wk10/test_eigenanalysis.py
+++ wk10/test_eigenanalysis.py
@@ -1,5 +1,4 @@
 import unittest
-from pprint import pprint
 
 import eigenanalysis as ea
 import linear_algebra as la
@@ -50,56 +49,49 @@ class TestEigenAnalysis(unittest.TestCase):
                 self.assertAlmostEqual(x1TAx1[i_row][j_column], 0.0)
 
     def test_cholesky_decomposition_00(self):
-        A = [[4.0, 0.5, 0.2],
-             [0.5, 2.0, 1.0],
-             [0.2, 1.0, 3.0]]
+        A = [[16.0, 12.0,  4.0,],
+             [12.0, 34.0, 13.0,],
+             [ 4.0, 13.0, 41.0,]]
 
-        L_expected = [[2.0, 0.0, 0.0],
-                      [0.25, 1.3919410907075056, 0.0],
-                      [0.1, 0.700460677904422, 1.580934799006486]]
+        L_expected = ((4, 0, 0),
+                      (3, 5, 0),
+                      (1, 2, 6),)
 
         L = ea.cholesky_decomposition(A)
-        print("Cholesky Decomposition:")
-        pprint(L)
 
         A_expected = la.multiply_matrix_matrix(L, zip(*L))
-        print("L LT:")
-        pprint(A_expected)
 
         # check size
         self.assertEqual(len(A), len(L))
 
+        # check row
         for i_row in xrange(0, len(A)):
             self.assertEqual(len(A), len(L[i_row]))
             for j_column in xrange(0, len(A)):
                 self.assertAlmostEqual(A[i_row][j_column], A_expected[i_row][j_column])
+                self.assertAlmostEqual(L_expected[i_row][j_column], L[i_row][j_column])
 
     def test_cholesky_decomposition_01(self):
-        A = [[64.0, 32.0, 2.0, 1.0],
-             [32.0, 128.0, 16.0, 4.0],
-             [2.0, 16.0, 256.0, 8.0],
-             [1.0, 4.0, 8.0, 512.0]]
+        L_expected = [[10.0, 0.0, 0.0, 0.0],
+                      [4.0, 9.0, 0.0, 0.0],
+                      [3.0, 5.0, 8.0, 0.0],
+                      [1.0, 2.0, 6.0, 7.0]]
 
-        L_expected = [[8.0, 0.0, 0.0, 0.0],
-                      [4.0, 10.583005244258363, 0.0, 0.0],
-                      [0.25, 1.417366773784602, 15.935136379352748, 0.0],
-                      [0.125, 0.3307189138830738, 0.47065803652096727, 22.619758641786127]]
+        A = la.multiply_matrix_matrix(L_expected, zip(*L_expected))
 
         L = ea.cholesky_decomposition(A)
-        print("Cholesky Decomposition:")
-        pprint(L)
 
         A_expected = la.multiply_matrix_matrix(L, zip(*L))
-        print("L LT:")
-        pprint(A_expected)
 
         # check size
         self.assertEqual(len(A), len(L))
 
+        # check row
         for i_row in xrange(0, len(A)):
             self.assertEqual(len(A), len(L[i_row]))
             for j_column in xrange(0, len(A)):
                 self.assertAlmostEqual(A[i_row][j_column], A_expected[i_row][j_column])
+                self.assertAlmostEqual(L_expected[i_row][j_column], L[i_row][j_column])
 
     def test_general_eigenproblem_symmetric_00(self):
         """
