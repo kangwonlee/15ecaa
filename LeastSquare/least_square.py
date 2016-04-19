f9d520e3b43d16aaa389faae3fe8e2b4122c7ccf
Kangwon Lee <kangwon.lee@kpu.ac.kr>
2016-04-19 19:01:04 +0900

    refactored generate_data() generate_plot()

--- LeastSquare/least_square.py
+++ LeastSquare/least_square.py
@@ -17,13 +17,12 @@ def normal_samples(n, mu = 0.0, sigma = 0.1):
 
 
 def main():
-    a = 0.5
-    b = 1.0
-    x_list = [x * 1.0 for x in range(10)]
-    y_list = [a * x + b for x in x_list]
-    noise = normal_samples(len(x_list), sigma=1)
-    y_contaminated = [y + w for y, w in zip(y_list, noise)]
+    x_list, y_contaminated, y_list = generate_data()
 
+    generate_plot(x_list, y_contaminated, y_list)
+
+
+def generate_plot(x_list, y_contaminated, y_list):
     pylab.plot(x_list, y_list, '.', label='true')
     pylab.plot(x_list, y_contaminated, '.', label='contaminated')
     pylab.grid()
@@ -32,6 +31,16 @@ def main():
     pylab.axis('equal')
     pylab.legend(loc=0)
     pylab.show()
+
+
+def generate_data():
+    a = 0.5
+    b = 1.0
+    x_list = [x * 1.0 for x in range(10)]
+    y_list = [a * x + b for x in x_list]
+    noise = normal_samples(len(x_list), sigma=1)
+    y_contaminated = [y + w for y, w in zip(y_list, noise)]
+    return x_list, y_contaminated, y_list
 
 
 if __name__ == '__main__':
