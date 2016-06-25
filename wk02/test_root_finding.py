c6f899919a46b610ce0de382be89d0cf0af40264
Kangwon Lee <kangwon.lee@kpu.ac.kr>
2016-06-25 19:39:55 +0900

    test_root_finding.py : added test for Newton-Raphson method

--- wk02/test_root_finding.py
+++ wk02/test_root_finding.py
@@ -8,6 +8,12 @@ def f(x):
     return (x - 1.0) * (x * x + x + 1.0)
 
 
+def df_dx(x):
+    """derivative"""
+    x = float(x)
+    return (x * x + x + 1.0) + (2 * x + 1.0)
+
+
 class TestRootFinding(unittest.TestCase):
     def test_sequential(self):
         result = rf.sequential(f, 0.1)
@@ -16,6 +22,11 @@ class TestRootFinding(unittest.TestCase):
 
     def test_bisection(self):
         result = rf.bisection(f, 0.0, 1.1)
+        expected = 1.0
+        self.assertAlmostEqual(result, expected, delta=0.001)
+
+    def test_newton_raphson(self):
+        result = rf.newton(f, df_dx, 0.0)
         expected = 1.0
         self.assertAlmostEqual(result, expected, delta=0.001)
 
