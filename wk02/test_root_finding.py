3e85d88260aa3ab348fcdd3112f60f585c2b0bb4
Kangwon Lee <kangwon.lee@kpu.ac.kr>
2016-06-25 19:34:36 +0900

    started test_root_finding.py : added sequential method

---
+++ wk02/test_root_finding.py
@@ -0,0 +1,19 @@
+import unittest
+
+import root_finding as rf
+
+
+def f(x):
+    x = float(x)
+    return (x - 1.0) * (x * x + x + 1.0)
+
+
+class TestRootFinding(unittest.TestCase):
+    def test_sequential(self):
+        result = rf.sequential(f, 0.1)
+        expected = 1.0
+        self.assertAlmostEqual(result, expected, delta=0.001)
+
+
+if __name__ == '__main__':
+    unittest.main()
