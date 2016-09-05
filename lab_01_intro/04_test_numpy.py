2a6628c15ec3cdd64b14b82f253f968c5f6ed6ae
KangWon LEE <kangwon.lee@kpu.ac.kr>
2016-09-05 22:13:33 +0900

    references for lab 01 files

--- lab_01_intro/04_test_numpy.py
+++ lab_01_intro/04_test_numpy.py
@@ -4,8 +4,11 @@ import numpy as np
 
 r1 = np.array((1.0, 2.0))
 print "r1 =", r1
+
 r2 = np.array((-2.0, 1.0))
 print "r2 =", r2
 
 r3 = r1 + r2
 print "r3 =", r3
+
+# Numpy Quickstart tutorial, https://docs.scipy.org/doc/numpy-dev/user/quickstart.html
